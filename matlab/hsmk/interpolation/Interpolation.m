1105330c5fb3229ae977f72b5ece10ac9098a424
hatsusakuramiku <hcj13794190602@gmail.com>
2023-10-12 00:33:52 +0800

    重构TaylorSeries构造方法与interpolation.factorial()方法，添加了 部分注释，修改了部分代码逻辑

--- matlab/hsmk/interpolation/Interpolation.m
+++ matlab/hsmk/interpolation/Interpolation.m
@@ -5,24 +5,32 @@ classdef Interpolation
         polynomial; % 拟合多项式
         fittingError; % 拟合误差
     end
-    properties (Constant)
-        x = sym('x');
+
+    properties (Constant,Access = protected)
+        x = sym('x'); % 常量
     end
 
-    properties(Dependent)
+    properties(Dependent,Access = protected)
         tempFittingFunction; % 临时拟合函数
     end
 
     methods
         function tempFittingFunction = get.tempFittingFunction(interpolation)
+            % 修改输入的公式的符号以方便计算
+            % @Attribute #tempFittingFunction 的值依赖于 @Attribute #fittingFunction
+            % 类中调用 @Attribute #tempFittingFunction 时检查 @Attribute #fittingFunction 的值，如果 @Attribute #fittingFunction 为空则 @Attribute #tempFittingFunction 为空
+            % 如果 @Attribute #fittingFunction 不为空则 使用"sym('x')"替换 @Attribute #fittingFunction 中的符号并赋值给 @Attribute #tempFittingFunction
             if ~isempty(interpolation.fittingFunction)
                 tempFittingFunction = subs(interpolation.fittingFunction,symvar(interpolation.fittingFunction),interpolation.x);
             else
                 tempFittingFunction = [];
             end
         end
+    end
 
+    methods(Access = public)
         function interpolation = Interpolation(fittingFunction, order)
+            % @Class #Interpolation 的构造函数
             interpolation.fittingFunction = [];
             interpolation.order = [];
             if abs(nargin) == 1
@@ -38,7 +46,6 @@ classdef Interpolation
                 error("输入参数过多！")
             end
         end
-
     end
 
     methods
@@ -51,9 +58,9 @@ classdef Interpolation
             interpolation.fittingFunction = fittingFunction;
         end
 
-        % function tempFittingFunction = getTempFittingFunction(interpolation)
-        %     tempFittingFunction = interpolation.tempFittingFunction;
-        % end
+        function tempFittingFunction = getTempFittingFunction(interpolation)
+            tempFittingFunction = interpolation.tempFittingFunction;
+        end
 
         function polynomial = getPolynomial(interpolation)
             polynomial = interpolation.polynomial;
@@ -73,63 +80,106 @@ classdef Interpolation
         function fittingError = getFittingError(interpolation)
             fittingError = interpolation.fittingError;
         end
-
     end
+
     methods(Access = protected)
-        function sum = factorial(interpolation,n,xCoordinate,option)
+        function sum = factorial(interpolation, n, xCoordinate, varargin)
+            % 类内部函数,用于计算连乘或阶乘
+            numArgs = abs(nargin);
             sum = sym(1);
-            switch(abs(nargin))
+            switch numArgs
+                case 1
+                    % 使用"interpolation.factorial"会至少输入一个参数"@Param #interpolation"
+                    % 如果仅有一个输入参数就输出一个空的矩阵或数组
+                    waring("无额外参数输入，未进行计算，将返回一个空的矩阵或数组！")
+                    sum = [];
                 case 2
+                    % 调用MATLAB中"factorial(n)"计算"n"的阶乘，其函数已有检错功能，此处不再另加
                     sum = sum .* factorial(n);
                 case 3
-                    if isempty(n)
+                    % 此处"n"为输入的 @Param #xCoordinate 的长度 - 1，即输入的x坐标向量中元素个数 - 1，"k"为输入参数 @Param #n，其中$ x_{i} $ 是@Param #xCoordinate 中的元素
+                    % 如果@Param #n为空，则计算 $  \prod_{ i = 0 }^{n} (x-x_{i})  $
+                    % 如果@Param #n不为空，则计算 $ \prod_{\begin{matrix} i = 0\\ i\ne k \end{matrix}}^{n} (x-x_{i}) $
+                    judgement = prejudgeN(n,xCoordinate);
+                    if judgement == 0
                         for i = 1:length(xCoordinate)
                             sum = sum * (interpolation.x - xCoordinate(i));
                         end
                         sum = simplify(sum);
                         return
-
-                    elseif  numel(n) ~= 1 || n <= 0 || fix(n) ~= n
-                        error("输入参数错误！ 请确保输入参数的非负整数！")
-                    elseif n > length(xCoordinate)
-                        error("超出数组索引！")
-                    end
-                    if length(unique(xCoordinate)) ~= length(xCoordinate)
-                        warning("数组中存在两个或多个相同元素，可能导致返回值为0！")
-                    end
-                    for i = 1:length(xCoordinate)
-                        if i == n
-                            continue
-                        end
-                        sum = sum * (xCoordinate(n) - xCoordinate(i));
-                    end
-                case 4
-                    if isequal(option,'exceptN')
-                        if isempty(n)
-                            for i = 1:length(xCoordinate)
-                                sum = sum * (interpolation.x - xCoordinate(i));
-                            end
-                            sum = simplify(sum);
-                            return
-
-                        elseif  numel(n) ~= 1 || n <= 0 || fix(n) ~= n
-                            error("输入参数错误！ 请确保输入参数的非负整数！")
-                        elseif n > length(xCoordinate)
-                            error("超出数组索引！")
-                        elseif length(unique(xCoordinate)) ~= length(xCoordinate)
-                            warning("数组中存在两个或多个相同元素，可能导致返回值为0！")
-                        end
-                        for i = 1:length(xCoordinate)
-                            if i == n
-                                continue
-                            end
+                    else
+                        for i = setdiff(1:length(xCoordinate), n)
                             sum = sum * (interpolation.x - xCoordinate(i));
                         end
+                    end
+                case 4
+                    % 此处"n"为输入的 @Param #xCoordinate 的长度 - 1，即输入的x坐标向量中元素个数 - 1，"k"为输入参数 @Param #n ,其中$ x_{i} $ 是@Param #xCoordinate 中的元素
+                    % 可输入 1 个可选参数
+                    % 可选参数及用法
+                    % ExceptN 计算 $ \prod_{\begin{matrix} i = 0\\ i\ne k \end{matrix}}^{n} (x_{k}-x_{i}) $
+                    % SerialProd 依次计算输入数组的前 n 项之积 ，即输入数组"A = [arg1,arg2,arg3], n = 3 ",返回值就为"sum = arg1 * arg 2 * arg 3"，如输入 n 值为空默认输出第一个
+                    % SerialProdAll 依次计算输入数组的前 n 项之积组成的数组 ，即输入数组"A = [arg1,arg2,arg3], n = 3 ",返回值就为"sum = [arg1, arg1 * arg 2, arg1 * arg 2 * arg 3]",如输入 n 值为空就计算全部
+                    option = {'ExceptN','SerialProd','SerialProdAll'}; % 暂时仅 2 个可选参数，后续可能会添加其他可选参数
+                    judgement = prejudgeN(n,xCoordinate);
+                    if isempty(varargin)
+                        warning("输入可选参数为空，将使用输入的 前三个参数进行计算！")
+                        sum = factorial(interpolation, n, xCoordinate);
                     else
-                        error("可选参数输入错误! ")
+                        if ~length(varargin) == 1
+                            error("输入可选参数过多，只能接受一个可选参数！");
+                        elseif ismember(varargin{1},option)
+                            index = find(A == varargin{1});
+                            switch(index)
+                                case 1
+                                    if judgement == 0
+                                        error("输入数组索引为空！")
+                                    else
+                                        for i = setdiff(1:length(xCoordinate), n)
+                                            sum = sum * (xCoordinate(n) - xCoordinate(i));
+                                        end
+                                    end
+
+                                case 2
+                                    if judgement == 0
+                                        sum = xCoordinate(1);
+                                    else
+                                        sum = pord(xCoordinate(1:n));
+                                    end
+
+                                case 3
+                                    if judgement == 0
+                                        len = length(xCoordinate);
+                                    else
+                                        len = n;
+                                    end
+                                    sumVector = sym(ones(1,len));
+                                    for i = 1:1:length(sumVector)
+                                        sumVector(i) = pord(xCoordinate(1:i));
+                                    end
+                                    sum = sum .* sumVector;
+
+                                otherwise
+                                    sum  = [];
+                            end
+                        else
+                            error("可选参数输入错误！");
+                        end
                     end
                 otherwise
                     error("输入的参数数目不正确！");
+            end
+        end
+
+        function judgement = prejudgeN(n,xCoordinate)
+            judgement = 1;
+            if isempty(n)
+                judgement = 0;
+            elseif  numel(n) ~= 1 || n <= 0 || fix(n) ~= n || n > length(xCoordinate)
+                error("输入参数错误或超出数组索引！ 请确保输入参数为非负整数且小于数组长度！")
+            else
+                if length(unique(xCoordinate)) ~= length(xCoordinate)
+                    warning("数组中存在两个或多个相同元素，可能导致返回值为0！")
+                end
             end
         end
     end
--- matlab/hsmk/interpolation/TaylorSeries.m
+++ matlab/hsmk/interpolation/TaylorSeries.m
@@ -1,12 +1,30 @@
 classdef TaylorSeries < Interpolation
 
     properties(Access = private)
-        center;
-        derivativeFunction;
+        center; % 展开式的中心
+        remainder; % 余项
+    end
+
+    properties(Dependent,Access = private)
+        derivativeFunction; % 导函数
     end
 
     methods
+        function derivativeFunction = get.derivativeFunction(taylorSeries)
+            if ~isempty(taylorSeries.fittingFunction) && ~isempty(taylorSeries.order)
+                derivativeFunction = sym(zeros(1,taylorSeries.order + 2)); % 创建一个细胞数组来存储导数函数
+                for n = 0:taylorSeries.order + 1
+                    derivativeFunction(n + 1) = diff(taylorSeries.tempFittingFunction, taylorSeries.x, n); % 求解第 n 阶导数，并存储在数组中
+                end
+            else
+                derivativeFunction = [];
+            end
+        end
+    end
+
+    methods(Access = public)
         function taylorSeries = TaylorSeries(fittingFunction,center,order)
+            % 构造函数
             switch(abs(nargin))
                 case 0
                     fittingFunction = [];
@@ -19,11 +37,26 @@ classdef TaylorSeries < Interpolation
                     order = [];
                 case 3
                 otherwise
-                    error("输入参数数量错误！")               
+                    error("输入参数数量错误！")
             end
-            if ~isempty(fittingFunction) && ~isempty(center) 
-                if any(order < 0 | ~isequal(length(symvar(fittingFunction)),1) | ~isfinite(subs(diff(fittingFunction,symvar(fittingFunction)),symvar(fittingFunction),center)))
-                    error("请确保展开阶数大于0且输入公式有且仅有一个自变量且输入公式在x_0处可导！")
+
+            if ~isempty(fittingFunction) && ~isempty(center)
+                if center ~= 0 || subs(1 / diff(fittingFunction,symvar(fittingFunction)),symvar(fittingFunction),center) ~= 0
+                    if any(order < 0 | ~isequal(length(symvar(fittingFunction)),1) | ~isfinite(subs(diff(fittingFunction,symvar(fittingFunction)),symvar(fittingFunction),center)))
+                        error("请确保展开阶数大于0，且输入公式有且仅有一个自变量，且输入公式在中心处可导！")
+                    end
+                else
+                    error("请确保展开阶数大于0，且输入公式有且仅有一个自变量，且输入公式在中心处可导！")
+                end
+            elseif  ~isempty(fittingFunction) && isempty(center)
+                warning("未输入展开式的中心值，将使用默认值'0'！");
+                center = 0;
+                if subs(1 / diff(fittingFunction,symvar(fittingFunction)),symvar(fittingFunction),center) ~= 0
+                    if any(order < 0 | ~isequal(length(symvar(fittingFunction)),1) | ~isfinite(subs(diff(fittingFunction,symvar(fittingFunction)),symvar(fittingFunction),center)))
+                        error("请确保展开阶数大于0，且输入公式有且仅有一个自变量，且输入公式在 x = 0 处可导！")
+                    end
+                else
+                    error("请确保展开阶数大于0，且输入公式有且仅有一个自变量，且输入公式在 x = 0 处可导！")
                 end
             end
             taylorSeries@Interpolation(fittingFunction,order)
@@ -39,26 +72,29 @@ classdef TaylorSeries < Interpolation
         end
 
         function derivativeFunction = getDerivativeFunction(taylorSeries)
-            if isempty(taylorSeries.derivativeFunction)
-                taylorSeries = taylorSeries.generateDerivativeFunction;
-            end
             derivativeFunction = taylorSeries.derivativeFunction;
+        end
+
+        function polynomial = getPolynomial(taylorSeries)
+            % 重写基类方法
+            if isempty(taylorSeries.polynomial)
+                taylorSeries = taylorSeries.generatePolynomial;
+            end
+            polynomial = taylorSeries.polynomial;
         end
 
     end
 
     methods(Access = private)
-        function taylorSeries = generateDerivativeFunction(taylorSeries)
-            syms x;
-            taylorSeries.derivativeFunction = sym(zeros(1,taylorSeries.order + 2)); % 创建一个细胞数组来存储导数函数
-            for n = 0:taylorSeries.order + 1
-                taylorSeries.derivativeFunction(n + 1) = diff(taylorSeries.tempFittingFunction, x, n); % 求解第 n 阶导数，并存储在数组中
-            end
-        end
-
         function taylorSeries = generatePolynomial(taylorSeries)
-            taylorSeries = taylorSeries.generateDerivativeFunction;
-            taylorSeries.polynomial = sum(taylorSeries.derivativeFunction ./ taylorSeries.factorial(0:1:(length(taylorSeries.derivativeFunction) - 1)));
+            syms x y;
+            len = length(taylorSeries.derivativeFunction);
+            polynomiaVector = (x - taylorSeries.center) .^ (0:1:len - 1);
+            tempDerivativeFunction = subs(taylorSeries.derivativeFunction(setdiff(1:len,len)),x,taylorSeries.center);
+            tempDerivativeFunction = [tempDerivativeFunction, subs(taylorSeries.derivativeFunction(end),x,y)];
+            polynomiaVector = (tempDerivativeFunction ./ taylorSeries.factorial(0:1:(len - 1))) .* polynomiaVector;
+            taylorSeries.polynomial = simplify(sum(polynomiaVector(1:(len - 1))));
+            taylorSeries.remainder = polynomiaVector(end);
         end
     end
 end
