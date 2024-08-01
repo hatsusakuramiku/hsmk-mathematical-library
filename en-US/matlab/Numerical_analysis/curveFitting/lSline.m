5c9a4ebce0aaabb25c40f1e61f98ba0bc8f0a09e
hatsusakuramiku <hcj13794190602@gmail.com>
2024-08-01 15:55:27 +0800

    删掉部分内容准备重写

--- en-US/matlab/Numerical_analysis/curveFitting/lSline.m
+++ en-US/matlab/Numerical_analysis/curveFitting/lSline.m
@@ -1,79 +1,87 @@
 function [polynomial, coefficients] = lSline(xCoordinate, yCoordinate, option)
-    %LSLINE 使用最小二乘法拟合直线
-    %   适用公式(此处使用Latex语法):
-    %   1. $y = Ax + B$ option = 0
-    %   2. $y = \frac{A}{x} + B$ option = 1
-    %   3. $y = \frac{D}{x + C}$ option = 2
-    %   4. $y = \frac{1}{Ax + B}$ option = 3
-    %   5. $y = Ce^{Ax}$ option = 4
-    %   6. $y = Cx^{A}$ option = 5
-    %   polynomial = LSLINE(xCoordinate, yCoordinate, option)
-    %   Inputs
-    %       xCoordinate - x坐标 {mustBeVector, mustBeReal}
-    %       yCoordinate - y坐标 {mustBeVector, mustBeReal}
-    %       option - 拟合方式 {mustBeMember(option, [0, 1, 2, 3, 4, 5, 6])}
-    %   Outputs
-    %       polynomial - 拟合直线方程
-    %       coefficients - 拟合曲线系数
-    %
-    %   详细文档参见：<a href="../../html/lSline_help.html">lSline</a>
-    %
+%LSLINE Uses least squares to fit a line
+%   [POLY, COEFFICIENTS] = LSLINE(XCOORDINATE, YCOORDINATE, OPTION) use least squares to fit a line
+%   with the given x and y coordinates.The option parameter specifies the line equation to fit.
+%
+%   Examples:
+%
+%   Accepts the following property/value pairs.
+%
+%       Input           Value/{Default}           Description
+%       -----------------------------------------------------------------------------------
+%       xCoordinate     vector {}                 Vector of x coordinates
+%       yCoordinate     vector {}                 Vector of y coordinates
+%       option          scalar integer {1}        Fit option mode
+%                                       1         $y = Ax + B$
+%                                       2         $y = \frac{A}{x} + B$
+%                                       3         $y = \frac{A}{x + B}$
+%                                       4         $y = \frac{1}{Ax + B}$
+%                                       5         $y = Be^{Ax}$
+%                                       6         $y = Bx^{A}$
+%                                                                                         .
+%       Output          Value/(Size)              Description
+%       -----------------------------------------------------------------------------------
+%       polynomial      sym scalar                Symbolic expression of the fitted line
+%       coefficients    vector(2)                 Coefficients of the fitted line (A, B)
+%
+%   See also: polyfit, polyval.
 
-    %% 输入参数校验
-    arguments
-        xCoordinate {mustBeVector, mustBeReal}
-        yCoordinate {mustBeVector, mustBeReal}
-        option {mustBeMember(option, [0, 1, 2, 3, 4, 5])} = 0
+%   Copyright 2024 HSMK.
+%% 输入参数校验
+arguments
+    xCoordinate {mustBeVector, mustBeReal}
+    yCoordinate {mustBeVector, mustBeReal}
+    option {mustBeMember(option, [0, 1, 2, 3, 4, 5])} = 0
+end
+
+% 输入参数个数校验
+narginchk(2, 3);
+
+% x 和 y 坐标长度校验
+if numel(unique(xCoordinate)) ~= numel(xCoordinate)
+    throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X坐标不唯一'));
+elseif size(xCoordinate, 1) ~= size(yCoordinate, 1)
+    throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X和Y坐标维度不一致'));
+elseif numel(xCoordinate) ~= numel(yCoordinate)
+    throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X和Y坐标长度不相等'));
+end
+
+try
+    
+    switch option
+        case 1
+            [xCoordinate, yCoordinate] = inv12Line(xCoordinate, yCoordinate);
+        case 2
+            [xCoordinate, yCoordinate] = inv22Line(xCoordinate, yCoordinate);
+        case 3
+            [xCoordinate, yCoordinate] = inv32Line(xCoordinate, yCoordinate);
+        case 4
+            [xCoordinate, yCoordinate] = ln12Line(xCoordinate, yCoordinate);
+        case 5
+            [xCoordinate, yCoordinate] = ln22Line(xCoordinate, yCoordinate);
     end
+    
+catch ME
+    throw(ME);
+end
 
-    % 输入参数个数校验
-    narginchk(2, 3);
-
-    % x 和 y 坐标长度校验
-    if numel(unique(xCoordinate)) ~= numel(xCoordinate)
-        throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X坐标不唯一'));
-    elseif size(xCoordinate, 1) ~= size(yCoordinate, 1)
-        throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X和Y坐标维度不一致'));
-    elseif numel(xCoordinate) ~= numel(yCoordinate)
-        throw(MException('MATLAB:LeastSquares:InvalidInput', '输入X和Y坐标长度不相等'));
-    end
-
-    try
-
-        switch option
-            case 1
-                [xCoordinate, yCoordinate] = inv12Line(xCoordinate, yCoordinate);
-            case 2
-                [xCoordinate, yCoordinate] = inv22Line(xCoordinate, yCoordinate);
-            case 3
-                [xCoordinate, yCoordinate] = inv32Line(xCoordinate, yCoordinate);
-            case 4
-                [xCoordinate, yCoordinate] = ln12Line(xCoordinate, yCoordinate);
-            case 5
-                [xCoordinate, yCoordinate] = ln22Line(xCoordinate, yCoordinate);
-        end
-
-    catch ME
-        throw(ME);
-    end
-
-    len = length(xCoordinate);
-    eX2 = sum(xCoordinate .^ 2);
-    eX = sum(xCoordinate);
-    eXY = sum(xCoordinate .* yCoordinate);
-    eY = sum(yCoordinate);
-    aMatrix = [eX2, eX; eX, len];
-    bVector = [eXY; eY];
-    result = linsolve(aMatrix, bVector);
-    coefficients = result;
-    polynomial = getPolynomial(result(1), result(2), option);
+len = length(xCoordinate);
+eX2 = sum(xCoordinate .^ 2);
+eX = sum(xCoordinate);
+eXY = sum(xCoordinate .* yCoordinate);
+eY = sum(yCoordinate);
+aMatrix = [eX2, eX; eX, len];
+bVector = [eXY; eY];
+result = linsolve(aMatrix, bVector);
+coefficients = result;
+polynomial = getPolynomial(result(1), result(2), option);
 
     function [xCoordinate, yCoordinate] = inv12Line(xCoordinate, yCoordinate)
         % y = A/x + B => y = A*(1/x) +B
         if ~isempty(xCoordinate(xCoordinate == 0))
             error('x不能等于0')
         end
-
+        
         xCoordinate = 1 ./ xCoordinate;
     end
 
@@ -87,7 +95,7 @@ function [polynomial, coefficients] = lSline(xCoordinate, yCoordinate, option)
         if ~isempty(yCoordinate(yCoordinate == 0))
             error('y不能等于0')
         end
-
+        
         yCoordinate = 1 ./ yCoordinate;
     end
 
@@ -96,7 +104,7 @@ function [polynomial, coefficients] = lSline(xCoordinate, yCoordinate, option)
         if ~isempty(yCoordinate(yCoordinate <= 0))
             error('y不能小于0')
         end
-
+        
         yCoordinate = log(yCoordinate);
     end
 
@@ -105,14 +113,14 @@ function [polynomial, coefficients] = lSline(xCoordinate, yCoordinate, option)
         if ~isempty(yCoordinate(yCoordinate <= 0)) || ~isempty(xCoordinate(yCoordinate <= 0))
             error('y和x不能小于或等于0')
         end
-
+        
         xCoordinate = log(xCoordinate);
         yCoordinate = log(yCoordinate);
     end
 
     function polynomial = getPolynomial(a, b, n)
         % 转换为符号表达式
-
+        
         switch n
             case 0
                 polynomial = @(x) a .* x + b;
@@ -127,7 +135,7 @@ function [polynomial, coefficients] = lSline(xCoordinate, yCoordinate, option)
             case 5
                 polynomial = @(x) exp(b) .* x .^ a;
         end
-
+        
     end
 
 end
